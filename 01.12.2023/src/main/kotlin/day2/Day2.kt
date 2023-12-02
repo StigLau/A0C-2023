4d544d588ed184485a3f637ba2533327bfa789a7
stiglau <stig@lau.no>
2023-12-02 13:13:51 +0100

    Day two Functional

--- 01.12.2023/src/main/kotlin/day2/Day2.kt
+++ 01.12.2023/src/main/kotlin/day2/Day2.kt
@@ -1,5 +1,6 @@
 package day2
 
+import java.lang.RuntimeException
 import java.net.URI
 import java.nio.file.Path
 import kotlin.io.path.readLines
@@ -11,10 +12,16 @@ class Day2 {
 fun main() {
     val uriz: URI = ClassLoader.getSystemResource("day2.txt").toURI()
     var tally = 0
+    var tally2 = 0
 
     val result = Path.of(uriz).readLines().map {
         try {
-            val values = parseGame(it.trim()) //Fetch colour
+            //Game2
+            tally2 += evaluateHand2(parseGame(it, false))
+
+            //Game 1
+            val gots = parseGame(it, true) //Fetch colour
+            val values =  it.split(":")[0].split(" ")[1].toInt()
             tally+=values
             println("Game success: $it")
 
@@ -24,12 +31,14 @@ fun main() {
 
     }
     println("final tally: $tally")
-
+    println("final tally2: $tally2")
+    assert(2683 == tally)
+    assert(49710 == tally2)
 }
 
 val lookup = mapOf("red" to 12, "green" to 13, "blue" to 14)
 
-private fun parseGame(it: String): Int {
+private fun parseGame(it: String, exitOnErrors:Boolean): List<List<Pair<String, Int>>> {
     val hands = it.split(":")[1] //Remove Game ID
         .split(";")
 
@@ -40,13 +49,12 @@ private fun parseGame(it: String): Int {
             val colour = draw[1]
 
             //Verify by the rules
-            if(value > lookup.get(colour)!!) {
+            if(exitOnErrors && value > lookup.get(colour)!!) {
                 println("Colour $colour was $value")
                 throw Exception()
             }
 
-            else
-                Pair(colour, value)
+            Pair(colour, value)
         }
         println(foot)
         if(foot.sumOf { it.second } > (12+13+14)) {
@@ -54,7 +62,23 @@ private fun parseGame(it: String): Int {
         }
         foot
     }
-    println("gots $gots")
-    val gameId = it.split(":")[0].split(" ")[1]
-    return gameId.toInt()
+    return gots
 }
+
+private fun evaluateHand2(gots: List<List<Pair<String, Int>>>): Int {
+    val green = extract(gots, "green")
+    val red = extract(gots, "red")
+    val blue = extract(gots, "blue")
+
+    val summary = green * red * blue
+    if (summary == 0) {
+        throw RuntimeException("Be warned, shouldn't have an empty colour")
+    }
+    return summary
+}
+
+private fun extract(
+    gots: List<List<Pair<String, Int>>>,
+    colour: String
+): Int = gots.flatMap { it }.filter { it.first == colour }.map { it.second }.max()
+
