b80625faa6655802fe84f88bc4cf22a9e39be214
stiglau <stig@lau.no>
2023-12-03 21:27:02 +0100

    Base code ready

--- 01.12.2023/src/test/kotlin/TestingDayThree.kt
+++ 01.12.2023/src/test/kotlin/TestingDayThree.kt
@@ -2,12 +2,15 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class TestingDayThree {
+    val line1 =
+        "..172..............................454..46.......507..........809......923.778..................793..............137.............238........"
+    val line2 =
+        "............*.........712........=.......*................515.*...........*.......690.........../..........658.........=.........*.........."
+    val line3 =
+        ".........823.835........%.........710.....749........134..%............................#812...&.....925.../..........276.......386.........."
+
     @Test
     fun testParsingLine() {
-        val line1 = "..172..............................454..46.......507..........809......923.778..................793..............137.............238........"
-        val line2 = "............*.........712........=.......*................515.*...........*.......690.........../..........658.........=.........*.........."
-        val line3 = ".........823.835........%.........710.....749........134..%............................#812...&.....925.../..........276.......386.........."
-
         assertEquals(10, foCruftyStuff("""\d+""".toRegex(), line1).size)
         assertEquals(8, foCruftyStuff("""[*=/%@+&\-$]""".toRegex(), line2).size)
 
@@ -15,7 +18,44 @@ class TestingDayThree {
         assertEquals(4, foCruftyStuff("""[*=/%@+&\-$]""".toRegex(), line3).size)
     }
 
-    private fun foCruftyStuff(regex: Regex, input: String): MutableList<Pair<String, IntRange>> {
+    @Test
+    fun testThreeLines() {
+        val digitRegex = """\d+""".toRegex()
+        val foundSymbolsIneMiddle = foCruftyStuff("""[*=/%@+&\-$]""".toRegex(), line2)
+
+        val upperRanges = asPairs(foCruftyStuff(digitRegex, line1))
+        val sameLineRanges = asPairs(foCruftyStuff(digitRegex, line2))
+        val lowerRanges = asPairs(foCruftyStuff(digitRegex, line3))
+        //val ranges = listOf(lowerRanges, sameLineRanges, upperRanges)
+        val rezzie = mutableSetOf<Pair<String, IntRange>>()
+        foundSymbolsIneMiddle.forEach {
+            //rezzie.addAll(ranges.map {ut -> extractOst(it.second, ut) })
+            rezzie.addAll(findInRange(it.second, lowerRanges))
+            rezzie.addAll(findInRange(it.second, upperRanges))
+            rezzie.addAll(findInRange(it.second, sameLineRanges))
+        }
+        println(rezzie)
+        assertEquals(12, rezzie.size)
+        assertEquals(7366, rezzie.map { it.first.toInt() }.sum())
+    }
+
+    private fun asPairs(lineFinds: List<Pair<String, IntRange>>): List<Pair<String, IntRange>> =
+        lineFinds.map { pair ->
+            Pair(pair.first, IntRange(pair.second.first - 1, pair.second.last + 1))
+    }
+
+    private fun findInRange(symbol: IntRange, lowerRanges: List<Pair<String, IntRange>>): MutableList<Pair<String, IntRange>> {
+        val tempList = mutableListOf<Pair<String, IntRange>>()
+        lowerRanges.filter { lows ->
+            lows.second.contains(symbol.endInclusive)
+        }.forEach {
+            tempList.add(it)
+        }
+        return tempList
+    }
+
+
+    private fun foCruftyStuff(regex: Regex, input: String): List<Pair<String, IntRange>> {
         val rez = mutableListOf<Pair<String, IntRange>>()
         regex.findAll(input).forEach { matchResult ->
             rez.add(Pair(matchResult.groups[0]!!.value, matchResult.groups[0]!!.range))
@@ -23,7 +63,4 @@ class TestingDayThree {
         return rez
     }
 
-    fun testThreeLines() {
-
-    }
 }
