2209621b59e03cc69f1f25148092455ac692a63d
stiglau <stig@lau.no>
2023-12-05 14:01:06 +0100

    Heapy spacy

--- 01.12.2023/src/main/kotlin/Day4.kt
+++ 01.12.2023/src/main/kotlin/Day4.kt
@@ -27,3 +27,49 @@ fun worth(numbers: List<String>, winners: List<String>): List<String> =
         numbers.contains(winner)
     }
 
+
+class Moro(val lookup: Map<Int, List<String>>) {
+
+    companion object {
+        fun createLookup(lines: List<String>): Map<Int, List<String>> {
+            val lookup = mutableMapOf<Int, List<String>>()
+            lines.mapIndexed { i, line ->
+                lookup.put(i, parse(line).map { it })
+            }.toList()
+            return lookup
+        }
+    }
+
+    fun searchInLines(lineNr:Int, lines: List<String>): List<String> {
+        val whatWeGot = lines.mapIndexed { outerIndex, line ->
+            //println("Child $lineNr $outerIndex")
+            //parse(line).mapIndexed { index, number ->
+            val rangez = IntRange(lineNr + 1, lineNr + (lines.size+1))
+            rangez.filter { it < 198 } //TODO Avoid going past 198!
+                .map { tryThisOne ->
+                    findNext(tryThisOne ) }
+        }
+        val withCurrent = mutableListOf("$lineNr")
+        withCurrent.addAll(whatWeGot.flatMap { it }.flatMap { it })
+        return withCurrent
+    }
+
+    fun findNext(nextLineToTry: Int): List<String> {
+        val found = lookup.get(nextLineToTry)!!
+            .filter { it.toInt() > nextLineToTry }
+            .filter { it.toInt() < 70 } //TODO This is where we control size of stuff
+        //50 762
+        //60 843
+
+        if(found.size == 0) {
+            //println("Found $nextLineToTry")
+            return listOf(nextLineToTry.toString())
+        }else { //Todo To be removed
+            //println("Founds $found")
+            return found.map { current ->
+                searchInLines(current.toInt(), lookup.get(current.toInt())!!
+                )//.filter { it > current })
+            }.flatMap { it }
+        }
+    }
+}
