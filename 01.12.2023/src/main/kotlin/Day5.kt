4f979f0c62ea62bc7a1ee01750702ef1b98f527b
stiglau <stig@lau.no>
2023-12-06 16:18:24 +0100

    Day 5 successful. Parts 1 & 2

--- 01.12.2023/src/main/kotlin/Day5.kt
+++ 01.12.2023/src/main/kotlin/Day5.kt
@@ -1,19 +1,14 @@
 class Day5(
-    val seed2Soil: List<Pair<IntRange, IntRange>>,
-    val soil2Fertilizer: List<Pair<IntRange, IntRange>>,
-    val fertilizer2Water: List<Pair<IntRange, IntRange>>,
-    val water2Light: List<Pair<IntRange, IntRange>>,
-    val light2Temperature: List<Pair<IntRange, IntRange>>,
-    val temperature2Humidity: List<Pair<IntRange, IntRange>>,
-    val humidity2Location: List<Pair<IntRange, IntRange>>,
+    val seed2Soil: List<Pair<MyRange, MyRange>>,
+    val soil2Fertilizer: List<Pair<MyRange, MyRange>>,
+    val fertilizer2Water: List<Pair<MyRange, MyRange>>,
+    val water2Light: List<Pair<MyRange, MyRange>>,
+    val light2Temperature: List<Pair<MyRange, MyRange>>,
+    val temperature2Humidity: List<Pair<MyRange, MyRange>>,
+    val humidity2Location: List<Pair<MyRange, MyRange>>,
 ) {
 
-    //seed-to-soil map:
-    //50 98 2
-    //52 50 48
-
-
-    fun findLocationForSeed(seedNr: Int): Int {
+    fun findLocationForSeed(seedNr: Long): Long {
         val soilId = lookupLookup(seedNr, seed2Soil)
         val fertilizerId = lookupLookup(soilId, soil2Fertilizer)
         val waterId = lookupLookup(fertilizerId, fertilizer2Water)
@@ -21,34 +16,61 @@ class Day5(
         val temperature = lookupLookup(lightId, light2Temperature)
         val humidity = lookupLookup(temperature, temperature2Humidity)
         val location = lookupLookup(humidity, humidity2Location)
+
         return location
     }
 
     //First is destination
     //Second is source
-    fun lookupLookup(sourceId: Int, lookupMap: List<Pair<IntRange, IntRange>>): Int {
+    fun lookupLookup(sourceId: Long, lookupMap: List<Pair<MyRange, MyRange>>): Long {
         lookupMap.filter { it.second.contains(sourceId) }.forEach {
-            //println(it)
-            val index:Int = it.second.indexOf(sourceId)
+            val index = sourceId-it.second.first
             return it.first.first + index
         }//Alternative
-        println("Didn't find $sourceId")
         return sourceId
 
     }
 
-    fun findLowest(seeds: List<String>): Int = seeds.map { findLocationForSeed(it.toInt()) }.min()
+    //fun findLowest(seeds: List<String>): Long = seeds.map { findLocationForSeed(it.toLong()) }.min()
+    fun findLowest(seeds: List<Long>): Long = seeds.map { findLocationForSeed(it) }.min()
+    fun findLowest(seeds: LongRange): Long = seeds.map { findLocationForSeed(it) }.min()
+
+    fun findLowest(seeds: MyRange): Long  {
+        val start = System.currentTimeMillis()
+        var lowest = Long.MAX_VALUE
+        var current = seeds.first
+        while (current < seeds.to) {
+            val rez = findLocationForSeed(current)
+            if(rez < lowest) {
+                println("${(System.currentTimeMillis() - start)} ms:  $rez")
+                lowest = rez
+            }
+            current++
+        }
+        return lowest
+    }
 
 
     companion object {
-        fun findPears(longerString: String) = longerString.lines().map { findPear(it) }
-        private fun findPear(input: String): Pair<IntRange, IntRange> {
+        fun pearHunt(longerString: String) = longerString.lines().map { findPear(it) }
+        private fun findPear(input: String): Pair<MyRange, MyRange> {
             //example Input = 50 98 2
             val numbers = input.trim().split(" ")
-            val dest = numbers[0].toInt()
-            val src = numbers[1].toInt()
-            val range = numbers[2].toInt()
-            return Pair(IntRange(dest, dest + range - 1), IntRange(src, src + range - 1))
+            val dest = numbers[0].toLong()
+            val src = numbers[1].toLong()
+            val range = numbers[2].toLong()
+            return Pair(MyRange(dest, dest + range - 1), MyRange(src, src + range - 1))
         }
+    }
+}
+
+
+data class MyRange(val first:Long, val to:Long) {
+
+    fun contains(doWeHaveThis:Long):Boolean = (first <= doWeHaveThis && doWeHaveThis <= to)
+
+    companion object {
+        fun fromRange(from:Long, range:Long):MyRange =
+            MyRange(from, (from + range -1))
     }
 }
