30244ddad13fba069f5a0e223805b3c9fcb42d09
stiglau <stig@lau.no>
2023-12-06 09:50:43 +0100

    Day 6 part 1 done

---
+++ 01.12.2023/src/main/kotlin/Day6.kt
@@ -0,0 +1,19 @@
+class Day6 {
+    //tine allowed for travel
+    //make sure you go farther in each race than the current record holder
+
+    //olding down the button charges the boat
+    //releasing the button allows the boat to move
+
+    //holding down the button, the boat's speed increases by one millimeter per millisecond
+//Which alternatives (times each other) wins?
+    companion object {
+        fun calculateBoatDistance(time: Int, timeToBeat: Int): List<Pair<Int, Int>> =
+            IntRange(1, time).map { a ->
+                Pair(a, a * (time - a))
+            }.filter { pear ->
+                pear.second > timeToBeat
+            }
+    }
+
+}
