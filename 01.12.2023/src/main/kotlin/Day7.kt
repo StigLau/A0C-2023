921a857e8a5b7f1cc0a7d48e7ba806ece9ab1221
stiglau <stig@lau.no>
2023-12-09 20:48:43 +0100

    Day7a is gone

--- 01.12.2023/src/main/kotlin/Day7.kt
+++ 01.12.2023/src/main/kotlin/Day7.kt
@@ -1,81 +1,76 @@
-class Day7 {
-    /*
-    fun parseInput(testdata: String): List<Hand> = testdata.lines().map {
-        val line = it.split(" ")
-        Hand(convertToHand(line[0]), line[1].toInt())
-    }*/
-    fun parseInput(lines:List<String>): List<Hand> = lines.map {
-        val line = it.split(" ")
-        Hand(convertToHand(line[0]), line[1].toInt())
-    }
+import java.lang.Exception
 
+class Day7
 
-    fun convertToHand(input: String): List<Int> =
-        input.map { labels.getValue(it) }.sortedDescending()
+data class Hand(val originalInput:String, val cards: List<Int>, val bid: Int) {
 
-    val labels: Map<Char, Int> = mapOf(
-        'A' to 14,
-        'K' to 13,
-        'Q' to 12,
-        'J' to 11,
-        'T' to 10,
-        '9' to 9,
-        '8' to 8,
-        '7' to 7,
-        '6' to 6,
-        '5' to 5,
-        '4' to 4,
-        '3' to 3,
-        '2' to 2
-    )
-}
+    //val gotStrength:Long = this.strength()
 
-data class Hand(val cards: List<Int>, val bid: Int) {
-
-    val gotStrength:Long = this.strength()
-
-    fun strength(): Long {
-        val grouped = cards.groupBy { it }
-        val groupCunts = grouped.map { it.key to  it.value.size }
-
-        val fiveOfAKind = howManyInAGroup(groupCunts, 5)
-        val four = howManyInAGroup(groupCunts, 4)
-        val three = howManyInAGroup(groupCunts, 3)
-        val pairs = howManyInAGroup(groupCunts, 2).sortedDescending()
-        val alone = howManyInAGroup(groupCunts, 1).sortedDescending()
-
-        return if(fiveOfAKind.isNotEmpty()) { //Five of a kind
-            70000000000L + fiveOfAKind.first()
-        } else if(four.isNotEmpty()) {//Four of a kind
-            60000000000L + (four.first() * 100) + alone.first()
-        } else if(three.isNotEmpty() && pairs.isNotEmpty()) { //Full house
-            50000000000L + (three.first() * 100) + pairs.first()
-        } else if(three.isNotEmpty()) { //Three of a kind
-            40000000000L + (three.first() * 10000) + (alone.get(0) * 100) + alone.get(1)
-        } else if(pairs.size == 2) { //Two pair
-            30000000000L + (pairs.first() * 10000) + (pairs.last()*100) + alone.first()
-        } else if(pairs.size == 1) { //One pair
-            20000000000L+(pairs.first()*1000000) + (alone.get(0)*10000) + (alone.get(1)*100) + alone.get(2)
-        } else //Highest
-            10000000000L + (cards.get(0) * 100000000L) + (cards.get(1) * 1000000) + (cards.get(2) * 10000) + (cards.get(3) * 100) + cards.get(4)
-    }
-
-    private fun howManyInAGroup(groupCunts: List<Pair<Int, Int>>, howMany: Int): List<Int> {
-        val fiveOfAKind = groupCunts.filter { it.second == howMany }.map { it.first }
-        return fiveOfAKind
-    }
-
-    fun haveYouSeenThisCard(thisCard:Int, rest:List<Int>):MutableList<Int> {
-        if(rest.isEmpty())
-            return mutableListOf()
-        else {
-            val filteredRest: MutableList<Int> = haveYouSeenThisCard(thisCard, rest.subList(1, rest.size))
-            if (rest.first() == thisCard) {
-                filteredRest.add(rest.first())
-            }
-            return filteredRest
+    companion object {
+        fun parseInput(lines:List<String>): List<Hand> = lines.map {
+            val line = it.split(" ")
+            Hand(line[0], convertToHand(line[0]), line[1].toInt())
         }
-    }
+        fun convertToHand(input: String): List<Int> =
+            input.map { labels.getValue(it) }.sortedDescending()
 
+        val labels: Map<Char, Int> = mapOf(
+            'A' to 14,
+            'K' to 13,
+            'Q' to 12,
+            'J' to 11,
+            'T' to 10,
+            '9' to 9,
+            '8' to 8,
+            '7' to 7,
+            '6' to 6,
+            '5' to 5,
+            '4' to 4,
+            '3' to 3,
+            '2' to 2
+        )
 
+        private fun howManyInAGroup(groupCunts: List<Pair<Int, Int>>, howMany: Int): List<Int> {
+            return groupCunts.filter { it.second == howMany }.map { it.first }
+        }
+
+        fun strength(cards: List<Int>): Int {
+            val grouped = cards.groupBy { it }
+            val groupCunts = grouped.map { it.key to it.value.size }
+
+            return if (howManyInAGroup(groupCunts, 5).isNotEmpty()) { //Five of a kind
+                7
+            } else if (howManyInAGroup(groupCunts, 4).isNotEmpty()) {//Four of a kind
+                6
+            } else if (howManyInAGroup(groupCunts, 3).isNotEmpty() && howManyInAGroup(groupCunts, 2).isNotEmpty()) { //Full house
+                5
+            } else if (howManyInAGroup(groupCunts, 3).isNotEmpty()) { //Three of a kind
+                4
+            } else if (howManyInAGroup(groupCunts, 2).size == 2) { //Two pair
+                3
+            } else if (howManyInAGroup(groupCunts, 2).size == 1) { //One pair
+                2
+            } else 1
+        }
+
+        val handComparator = object : Comparator<Hand> {
+            override fun compare(hmd1: Hand, hmd2: Hand): Int {
+                val diff = strength(hmd1.cards) - strength(hmd2.cards)
+                return if (diff != 0) {
+                    diff
+                } else {
+                    compareTwoBiatches(hmd1.originalInput, hmd2.originalInput)
+                }
+            }
+        }
+
+        private fun compareTwoBiatches(hmd1: String, hmd2: String): Int {
+            hmd1.forEachIndexed { index, i ->
+                val comparison = labels.get(hmd1[index])!! - labels.get(hmd2[index])!!
+                if (comparison != 0) {
+                    return comparison
+                }
+            }
+            throw Exception("Data says no")
+        }    }
 }
--- 01.12.2023/src/test/kotlin/TestingDay7.kt
+++ 01.12.2023/src/test/kotlin/TestingDay7.kt
@@ -29,7 +29,7 @@ AKQJT 41
     val day7 = Day7()
     @Test
     fun testingHandConversion() {
-        val hand = day7.convertToHand("T55J5")
+        val hand = Hand.convertToHand("T55J5")
         assertEquals(11, hand.first())
         assertEquals(5, hand.last())
         assertEquals(5, hand.size)
@@ -37,7 +37,7 @@ AKQJT 41
 
     @Test
     fun testInputParsing() {
-        val hands: List<Hand> = day7.parseInput(testdata.lines())
+        val hands: List<Hand> = Hand.parseInput(testdata.lines())
         assertEquals(5, hands.size)
         assertEquals(765, hands.get(0).bid)
         assertEquals(483, hands.get(4).bid)
@@ -45,27 +45,33 @@ AKQJT 41
 
     @Test
     fun testEvaluatingHand() {
-        val hands: List<Hand> = day7.parseInput(testdata.lines())
+        val hands: List<Hand> = Hand.parseInput(testdata.lines())
+        /*
         assertEquals(20003131002L, hands.get(0).strength())
         assertEquals(40000051110, hands.get(1).strength())
         assertEquals(30000130706, hands.get(2).strength())
         assertEquals(30000111013, hands.get(3).strength())
         assertEquals(40000121411, hands.get(4).strength())
+
+         */
     }
 
     @Test
     fun battleHands() {
+        /*
         val hands: List<Hand> = day7.parseInput(testdata.lines()).sortedBy { it.gotStrength }
         assertEquals(40000121411, hands.get(4).gotStrength)
         assertEquals(40000051110, hands.get(3).gotStrength)
         assertEquals(30000130706, hands.get(2).gotStrength)
         assertEquals(30000111013, hands.get(1).gotStrength)
         assertEquals(20003131002, hands.get(0).gotStrength)
-    }
 
+         */
+    }
+/*
     @Test
     fun handAndBid() {
-        val hands: List<Hand> = day7.parseInput(testdata.lines()).sortedBy { it.gotStrength }
+        val hands: List<Hand> = day7.parseInput(testdata.lines()).sortedBy { it.strength(it.cards) }
         assertEquals(483, hands.last().bid)
         assertEquals(765, hands.first().bid)
         val rankedHandsAndBids = hands.mapIndexed { index, hand ->
@@ -74,73 +80,39 @@ AKQJT 41
         assertEquals(6440, rankedHandsAndBids.sum())
     }
 
+ */
+
     @Test
     fun testWithTestData2() {
-        val hands = day7.parseInput(testdata2.lines())
-        val rankedHandsAndBids = hands.mapIndexed { index, hand ->
+        val hands = Hand.parseInput(testdata2.lines())
+        val sortedHands = hands.sortedWith(Hand.handComparator)
+        sortedHands.forEach { println(it) }
+        val rankedHandsAndBids = hands.sortedWith(Hand.handComparator).mapIndexed { index, hand ->
             (index + 1) * hand.bid }
 
         assertEquals(1343, rankedHandsAndBids.sum())
-        //
+    }
+
+
+    @Test
+    fun newTestWithSuperTestData() {
+        val hands = Hand.parseInput(testdata2.lines())
+
+        val sortedStuff = hands.sortedWith(Hand.handComparator)
+        val rankedHandsAndBids = sortedStuff.mapIndexed { index, hand ->
+            (index + 1) * hand.bid }
+        assertEquals(1343, rankedHandsAndBids.sum())
     }
 
     @Test
-    fun dealWithProd() {
-        val unsortedHands = day7.parseInput(readFile("day7.txt"))
-        val hands: List<Hand> = unsortedHands.sortedBy { it.gotStrength }
-        assertEquals(1000, unsortedHands.size)
-        assertEquals(460, unsortedHands.first().bid)
-        assertEquals(50000000910, unsortedHands.first().strength())
-        assertEquals(677, unsortedHands.last().bid)
-        assertEquals(60000001211, unsortedHands.last().strength())
-        assertEquals(396, hands.last().bid)
-        assertEquals(70000000011, hands.last().gotStrength)
-        assertEquals(1, hands.get(1).bid)
-        assertEquals(10807040302, hands.get(1).gotStrength)
-        assertEquals(556, hands.first().bid)
-        assertEquals(10706050302, hands.first().gotStrength)
-
-        var sumItALL:Long = 0
-        val rankedHandsAndBids = hands.mapIndexed { index, hand ->
-            sumItALL += (index + 1) * hand.bid
-            (index + 1) * hand.bid
-
-        }
-        assertEquals(247255339, rankedHandsAndBids.sum())
-        assertEquals(247255339, sumItALL)
-
-        //247411359 was too high!
-        //247375415 was wrong
-        //247374953 too high!
-        //250382021 was wrong
-        //250390839 <- Any better?
-        //253745161
-        //NOT 247255339
+    fun newProdTest() {
+        val hands = Hand.parseInput(readFile("day7.txt"))
+        val sortedStuff = hands.sortedWith(Hand.handComparator)
+        sortedStuff.forEach { println(it) }
+        val rankedHandsAndBids = sortedStuff.mapIndexed { index, hand ->
+            (index + 1) * hand.bid }
+        assertEquals(246795406, rankedHandsAndBids.sum())
     }
-
-    @Test
-    fun dealWithTheLesserValueCardsALSO() {
-        val hands: List<Hand> = day7.parseInput(readFile("day7.txt")).sortedByDescending { it.gotStrength }
-        //Read pears to verify then against each other
-        val filteredPairs = hands.filter {
-            //it.gotStrength > 100000000 &&
-                    it.gotStrength < 20000000000L }
-        println(filteredPairs)
-
-        //House fucks
-        // bids 85. 811 109
-        // 9, 10, 11
-        val rankedHandsAndBids = hands.mapIndexed { index, hand ->
-            (index + 1) * hand.bid
-        }
-        assertEquals(253745161, rankedHandsAndBids.sum())
-    }
-
-
-
-    ///////!!!!!!!TODO Husk på å finne ut av hvilken som har de høyeste verdiene basert på neste kort!
-
-
 
 
 
